32b6da00016f2999b165115848ebdf5c388a6e32
Horangel Millan <horangelmillan@outlook.com>
2022-06-21 19:19:44 -0500

    else if retired and optimized code in registrations controller

--- controllers/registrations.controller.js
+++ controllers/registrations.controller.js
@@ -22,27 +22,23 @@ const getAllRegistrations = async (req, res) => {
 const getRegistration = async (req, res) => {
     const { id } = req.params;
 
-    try {
-        const registration = await Registrations.findOne({
-            where: {
-                id
-            }
-        });
+    const registration = await Registrations.findOne({
+        where: {
+            id
+        }
+    });
 
-        if (registration) {
-            res.status(200).json({
-                status: 'success',
-                registration
-            });
-        } else if (registration === null) {
-            res.status(404).json({
-                status: 'Not found',
-                registration
-            });
-        };
-    } catch (err) {
-        console.log(err);
+    if (!registration) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'registration not found'
+        });
     };
+
+    res.status(200).json({
+        status: 'success',
+        registration
+    });
 };
 
 // create a enter registration
@@ -66,59 +62,51 @@ const createRegistration = async (req, res) => {
 const exitRegistration = async (req, res) => {
     const { id } = req.params;
 
-    try {
-        const exit = await Registrations.update({
-            exitTime: Date.now(),
-            status: 'out'
-        }, {
-            where: {
-                id
-            }
-        });
+    const exit = await Registrations.update({
+        exitTime: Date.now(),
+        status: 'out'
+    }, {
+        where: {
+            id
+        }
+    });
 
-        if (exit['0'] === 1) {
-            res.status(200).json({
-                status: 'success',
-                exit
-            });
-        } else if (exit['0'] === 0) {
-            res.status(404).json({
-                status: 'Not found',
-                exit
-            });
-        };
-    } catch (err) {
-        console.log(err);
+    if (exit['0'] != 1) {
+        res.status(400).json({
+            status: 'error',
+            message: 'registration not exist'
+        });
     };
+
+    res.status(200).json({
+        status: 'success',
+        exit
+    });
 };
 
 // cancel a exit registration
 const cancelRegistration = async (req, res) => {
     const { id } = req.params;
 
-    try {
-        const cancel = await Registrations.update({
-            status: 'cancelled'
-        }, {
-            where: {
-                id
-            }
-        });
+    const cancel = await Registrations.update({
+        status: 'cancelled'
+    }, {
+        where: {
+            id
+        }
+    });
 
-        if (cancel['0'] === 1) {
-            res.status(200).json({
-                status: 'success',
-                cancel
-            });
-        } else if (cancel['0'] === 0) {
-            res.status(404).json({
-                status: 'Not found',
-                cancel
-            });
-        };
-    } catch (err) {
-        console.log(err)
+    if (cancel['0'] != 1) {
+        res.status(400).json({
+            status: 'error',
+            message: 'registration not exist'
+        });
     };
+
+    res.status(200).json({
+        status: 'success',
+        cancel
+    });
 };
 
 module.exports = {
